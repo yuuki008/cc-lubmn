24aaf0e367a91b25d13da382bff2f316646736ee
yuuki008 <s22ah261@gmail.com>
2024-05-22 10:56:46 +0900

    vector

---
+++ section23/vector/Position2D.h
@@ -0,0 +1,13 @@
+#ifndef _POSITION2D_H_
+#define _POSITION2D_H_
+
+#include "Vector2D.h"
+
+class Position2D : public Vector2D
+{
+public:
+  void resetPosition();
+  void move(int dx, int dy);
+};
+
+#endif // _POSITION2D_H_
---
+++ section23/vector/Vector2D.cpp
@@ -0,0 +1,28 @@
+#include "Vector2D.h"
+
+Vector2D::Vector2D()
+{
+  init();
+}
+
+void Vector2D::setValue(int x, int y)
+{
+  m_x = x;
+  m_y = y;
+}
+
+int Vector2D::getX()
+{
+  return m_x;
+}
+
+int Vector2D::getY()
+{
+  return m_y;
+}
+
+void Vector2D::init()
+{
+  m_x = 0;
+  m_y = 0;
+}
